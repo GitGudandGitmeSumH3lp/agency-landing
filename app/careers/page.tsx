1fd80a32324effcd6e5c1175a34b9f466f175615
GitGudandGitmeSumH3lp <caloyy24@gmail.com>
2025-12-24 03:46:42 +0800

    possible deployment fix for api

---
+++ app/careers/page.tsx
@@ -0,0 +1,8 @@
+export default function CareersPage() {
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-32">
+      <h1 className="text-4xl font-bold mb-8">Careers</h1>
+      <p className="text-gray-600">We are currently not hiring, but check back soon!</p>
+    </div>
+  );
+}
